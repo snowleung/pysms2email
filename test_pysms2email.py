6d0f2760239e52039122e305abdcfc032ca6fe76
Samleung <callsamleung@gmail.com>
2017-03-04 21:39:40 +0800

    Fix test

--- test_pysms2email.py
+++ test_pysms2email.py
@@ -2,5 +2,5 @@ import sms2email
 
 def test_build_content():
     date = sms2email.message_date(1)
-    test_data = [(date, 'author', 'text')]
+    test_data = [(1, 'author', 'text')]
     assert '''Author:author\nTEXT:\ntext\n'''+str(date)+'''\n\n\n''' == sms2email.build_content(test_data)
