f513c218652000dc24fee14d37c82077eefa31e0
Samleung <callsamleung@gmail.com>
2017-03-21 17:46:00 +0800

    Fix test

--- tests/test_pysms2email.py
+++ tests/test_pysms2email.py
@@ -1,4 +1,4 @@
-import sms2email
+from pysms2email import sms2email
 
 def test_build_content():
     date = sms2email.message_date(1)
@@ -6,4 +6,7 @@ def test_build_content():
     assert '''Author:author\nTEXT:\ntext\n'''+str(date)+'''\n\n\n''' == sms2email.build_content(test_data)
 
 def test_run_diagnostic():
-    assert sms2email.run_diagnostic()
+    import os
+    path = os.path.split(os.path.realpath(__file__))[0]
+    diagnostic_file = os.path.join(path, '''./diagnostic''')
+    assert sms2email.run_diagnostic(diagnostic_file) == 'test is ok'
