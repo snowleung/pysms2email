f232bd2419ce85323e87574f03f0e3b72ace2ad6
Samleung <callsamleung@gmail.com>
2017-03-24 13:28:46 +0800

    Apply log

---
+++ pysms2email/simplelog.py
@@ -0,0 +1,8 @@
+# coding:utf-8
+
+import logging
+import logging.config
+
+logging.config.fileConfig('./simplelog.cfg')
+logger_pysms2email = logging.getLogger('pysms2email')
+logger_pysms2email.debug('logging init OK')
