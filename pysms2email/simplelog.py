81b2d03bf3900dd5f60865a9a3532c2016f0cb6b
Samleung <callsamleung@gmail.com>
2017-03-24 15:00:58 +0800

    fixed path

--- pysms2email/simplelog.py
+++ pysms2email/simplelog.py
@@ -3,7 +3,8 @@
 
 import logging
 import logging.config
+import os
 
-logging.config.fileConfig('./simplelog.cfg')
+logging.config.fileConfig(os.path.join(os.path.split(__file__)[0], 'simplelog.cfg'))
 logger_pysms2email = logging.getLogger('pysms2email')
 logger_pysms2email.info('logging init OK')
